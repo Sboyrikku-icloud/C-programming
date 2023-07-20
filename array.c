7d11e483412361561de06bd128514cba6d00d539
Ritim Sboy <70766889+Sboyrikku-icloud@users.noreply.github.com>
2023-07-20 12:51:40 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-int main ( ){
-   int a[5] = {10,20,30,40,50};
+int main ( )
+{
+   int a[5] = {10,20,30,40,50}; // arry initilze
    int i;
    printf ("elements of the array are");
    for ( i=0; i<5; i++)
